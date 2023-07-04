b2251d74180daed1fc2415f21d14ee68773e4d91
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-04 09:10:55 -0500

    Create put.js

---
+++ put.js
@@ -0,0 +1,96 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const server = require('../server');
+
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+suite('Functional Tests', function() {
+    this.timeout(5000);
+    suite('Integration tests with chai-http', function() {
+        // #1
+        test('Test GET /hello with no name', function(done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .get('/hello')
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.text, 'hello Guest');
+                    done();
+                });
+        });
+        // #2
+        test('Test GET /hello with your name', function(done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .get('/hello?name=xy_z')
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.text, 'hello xy_z');
+                    done();
+                });
+        });
+        // #3
+        test('Send {surname: "Colombo"}', function(done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .put('/travellers')
+                .send({ surname: "Colombo" })
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.type, 'application/json');
+                    assert.equal(res.body.name, 'Cristoforo');
+                    assert.equal(res.body.surname, 'Colombo');
+
+                    done();
+                });
+        });
+        // #4
+        test('send {surname: "da Verrazzano"}', function(done) {
+            chai.request(server)
+                .put('/travellers')
+                .send({ surname: "da Verrazzano" })
+                .end(function(err, res) {
+
+                    assert.equal(res.status, 200, 'response status should be 200');
+                    assert.equal(res.type, 'application/json', "Response should be json");
+                    assert.equal(res.body.name, 'Giovanni', 'res.body.name should be "Giovanni"');
+                    assert.equal(res.body.surname, 'da Verrazzano', 'res.body.surname should be "da Verrazzano"');
+
+                    done();
+                });
+        });
+    });
+});
+const Browser = require('zombie');
+
+suite('Functional Tests with Zombie.js', function() {
+    this.timeout(5000);
+
+
+
+    suite('Headless browser', function() {
+        test('should have a working "site" property', function() {
+            assert.isNotNull(browser.site);
+        });
+    });
+
+    suite('"Famous Italian Explorers" form', function() {
+        // #5
+        test('Submit the surname "Colombo" in the HTML form', function(done) {
+            assert.fail();
+
+            done();
+        });
+        // #6
+        test('Submit the surname "Vespucci" in the HTML form', function(done) {
+            assert.fail();
+
+            done();
+        });
+    });
+});
